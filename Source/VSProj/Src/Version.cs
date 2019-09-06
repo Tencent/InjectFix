3f34ca998b0e8625b84c4db4da6302912e1cbee8
johnche <johnche@tencent.com>
2019-09-06 12:35:59 +0800

    all sources: 'LICENSE.txt' -> 'LICENSE'

--- Source/VSProj/Src/Version.cs
+++ Source/VSProj/Src/Version.cs
@@ -1,8 +1,8 @@
 /*
  * Tencent is pleased to support the open source community by making InjectFix available.
  * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
- * InjectFix is licensed under the MIT License, except for the third-party components listed in the file 'LICENSE.txt' which may be subject to their corresponding license terms. 
- * This file is subject to the terms and conditions defined in file 'LICENSE.txt', which is part of this source code package.
+ * InjectFix is licensed under the MIT License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
+ * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
  */
 
 namespace IFix
