ed2ae06b43eb68fb49e2c8a5dbe3e9f8927a46c1
johnche <chexiongsheng@qq.com>
2019-09-10 14:22:30 +0800

    make script work for mac

--- Source/VSProj/build_for_unity.sh
+++ Source/VSProj/build_for_unity.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env sh
-UNITY_HOME="/Applications/Unity/Hub/Editor/2017.4.19f1/Unity.app"
+UNITY_HOME="/Applications/Unity2017/Unity.app"
 GMCS="$UNITY_HOME/Contents/Mono/bin/gmcs"
 MONO="$UNITY_HOME/Contents/MonoBleedingEdge/bin/mono"
 DLL_OUTPUT="../UnityProj/Assets/Plugins/IFix.Core.dll"
 TOOL_KIT_PATH="../UnityProj/IFixToolKit"
 
-$GMCS ShuffleInstruction.cs -out:.\ShuffleInstruction.exe
-$MONO ShuffleInstruction.exe Src\Core\Instruction.cs Instruction.cs
+$GMCS ShuffleInstruction.cs -out:./ShuffleInstruction.exe
+$MONO ShuffleInstruction.exe Src/Core/Instruction.cs Instruction.cs
 $GMCS -define:UNITY_IPHONE -unsafe -target:library -out:$DLL_OUTPUT Src/Builder/*.cs Src/Version.cs Instruction.cs \
-Src\Core\AnonymousStorey.cs \
-Src\Core\DataDefine.cs \
-Src\Core\GenericDelegate.cs \
-Src\Core\Il2CppSetOptionAttribute.cs \
-Src\Core\ObjectClone.cs \
-Src\Core\ReflectionMethodInvoker.cs \
-Src\Core\StackOperation.cs \
-Src\Core\SwitchFlags.cs \
-Src\Core\Utils.cs \
-Src\Core\VirtualMachine.cs \
-Src\Core\WrappersManager.cs
+Src/Core/AnonymousStorey.cs \
+Src/Core/DataDefine.cs \
+Src/Core/GenericDelegate.cs \
+Src/Core/Il2CppSetOptionAttribute.cs \
+Src/Core/ObjectClone.cs \
+Src/Core/ReflectionMethodInvoker.cs \
+Src/Core/StackOperation.cs \
+Src/Core/SwitchFlags.cs \
+Src/Core/Utils.cs \
+Src/Core/VirtualMachine.cs \
+Src/Core/WrappersManager.cs
 
 if [ ! -d $TOOL_KIT_PATH ]; then
     mkdir $TOOL_KIT_PATH
