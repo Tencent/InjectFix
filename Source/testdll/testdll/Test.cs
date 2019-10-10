4e1b10560f1b078eb70b07eabde64db09a3f7c43
onemore <treertzhu@123u.com>
2019-10-10 20:42:59 +0800

    测试使用 [Interpret]

--- Source/testdll/testdll/Test.cs
+++ Source/testdll/testdll/Test.cs
@@ -51,13 +51,34 @@ namespace testdll
 
         }
 
+        private void hhh(int a)
+        {
+
+        }
+
         //[Patch]
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+        //[Patch]
+        //public static int Min(int a, int b)
+        //{
+        //    return _Min(a, b);
+        //}
+
+        //[Interpret]
+        //static int _Min(int a, int b)
+        //{
+        //    return Math.Min(a, b);
+        //}
+
         public int Add(int a, int b)
         {
             return a + b + 1000;
         }
 
-        //[Patch]
         public static int Min(int a, int b)
         {
             return Math.Min(a, b) + 1000;
