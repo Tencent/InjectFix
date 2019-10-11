c34f5c8d727d4e9bb2b79e092b361c0414c7be5e
onemore <treertzhu@123u.com>
2019-10-11 20:38:23 +0800

    低于5.6的版本，mono执行也加上 --debug

--- Source/UnityProj/Assets/IFix/Editor/ILFixEditor.cs
+++ Source/UnityProj/Assets/IFix/Editor/ILFixEditor.cs
@@ -94,7 +94,7 @@ namespace IFix.Editor
 #if UNITY_5_6_OR_NEWER
             hotfix_injection.StartInfo.Arguments = "--debug --runtime=v4.0.30319 \"" + inject_tool_path + "\" \""
 #else
-            hotfix_injection.StartInfo.Arguments = "\"" + inject_tool_path + "\" \""
+            hotfix_injection.StartInfo.Arguments = "--debug \"" + inject_tool_path + "\" \""
 #endif
                 + string.Join("\" \"", args.ToArray()) + "\"";
             hotfix_injection.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
