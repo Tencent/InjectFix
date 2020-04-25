0a7da391839bb52390ea19a3a42253fcd3744faa
johnche <johnche@tencent.com>
2020-04-25 19:33:29 +0800

    添加新增类的演示例子

---
+++ Source/UnityProj/Assets/NewClass/VMBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VMBehaviourScript : MonoBehaviour
+{
+    public IMonoBehaviour VMMonoBehaviour { get;set; }
+
+    void Start()
+    {
+        if (VMMonoBehaviour != null)
+        {
+            VMMonoBehaviour.Start();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VMMonoBehaviour != null)
+        {
+            VMMonoBehaviour.Update();
+        }
+    }
+}
