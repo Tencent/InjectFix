9d8e95072a88f577e95ad581ea6082f654337b4c
xuyanghuang <xuyanghuang@tencent.com>
2022-01-17 18:33:31 +0800

    新增对泛型字段访问支持

--- Source/VSProj/Src/Tools/CodeTranslator.cs
+++ Source/VSProj/Src/Tools/CodeTranslator.cs
@@ -3853,6 +3853,16 @@ namespace IFix
                 for (int i = 0; i < fields.Count; i++)
                 {
                     var fieldType = fields[i].FieldType;
+                    if (fieldType.IsGenericParameter)
+                    {
+                        var resolveType = ((GenericParameter)fieldType).ResolveGenericArgument(fields[i].DeclaringType);
+                        if (resolveType != null)
+                        {
+                            addExternType(resolveType);
+                            continue;
+                        }
+                    }
+
                     if (isCompilerGenerated(fieldType) || isNewClass(fieldType as TypeDefinition))
                     {
                         fieldType = objType;
