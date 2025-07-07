5ba42eb8f63fe37134a3c78690f97d8425e13588
xuyanghuang <xuyanghuang@tencent.com>
2025-07-07 10:24:08 +0800

    更新LICENSE的公司主体

--- Source/VSProj/Src/TestDLL/RedirectBaseTest.cs
+++ Source/VSProj/Src/TestDLL/RedirectBaseTest.cs
@@ -1,6 +1,6 @@
 /*
  * Tencent is pleased to support the open source community by making InjectFix available.
- * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Copyright (C) 2019 Tencent.  All rights reserved.
  * InjectFix is licensed under the MIT License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
  * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
  */
